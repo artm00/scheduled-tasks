e16818b3d0f1fe7299faec03f3ea714ae19e68b7
artm00 <165009710+artm00@users.noreply.github.com>
2026-04-20 12:43:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+# # Munich
+# LAT = 48.135124
+# LON = 11.581981
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# Dresden
+LAT = 51.050407
+LON = 13.737262
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+
+params = {
+    "lat": LAT,
+    "lon": LON,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=params)
+response.raise_for_status()
+weather_data = response.json()
+
+# for i in range(4):
+#     code = weather_data["list"][i]["weather"][0]["id"]
+#     print(code)
+
+weather_id = [weather_data["list"][i]["weather"][0]["id"] for i in range(4)]
+
+will_it_rain = any([x < 700 for x in weather_id])
+yes_no_list = [x < 700 for x in weather_id]
+print(will_it_rain)
+if will_it_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It is goint to rain today, bring the umbrella!!!☔️",
+        from_="+17755875397",
+        to="+4915251724131",
+    )
+
+    print(message.status)
